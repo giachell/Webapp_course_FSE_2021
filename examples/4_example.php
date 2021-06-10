45e500d4d429857b8a7073b3cd18938cc0b80302
Fabio Giachelle <giachelle.fabio@gmail.com>
2021-06-11 00:52:37 +0200

    initial examples added

---
+++ examples/4_example.php
@@ -0,0 +1,23 @@
+<?php
+
+$title = "<h1> Conditional statements (if / else) </h1>"; // HTML string 
+
+$a = 5; // first number
+$b = 1; // second number
+
+echo $title;
+echo "a: $a; b: $b <br/><br/>";
+
+if ($a > $b)
+{
+	echo "a e` maggiore di b";
+}
+else if ($a == $b)
+{
+	echo "a e` uguale a b";
+}
+else
+{
+	echo "a e` minore di b";
+}
+?> 
